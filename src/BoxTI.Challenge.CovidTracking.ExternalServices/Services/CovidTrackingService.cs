fd55a0e81bed0cf1cb5db6e119017dc600e724fc
Pedro Pinheiro <phleitepinheiro@gmail.com>
2021-05-04 18:17:44 -0300

    feat: initial setup of external services layer

---
+++ src/BoxTI.Challenge.CovidTracking.ExternalServices/Services/CovidTrackingService.cs
@@ -0,0 +1,11 @@
+using System.Threading.Tasks;
+using BoxTI.Challenge.CovidTracking.ExternalServices.Interfaces;
+using BoxTI.Challenge.CovidTracking.ExternalServices.ViewModels;
+
+namespace BoxTI.Challenge.CovidTracking.ExternalServices.Services
+{
+    public class CovidTrackingService : ICovidTrackingService
+    {
+
+    }
+}
